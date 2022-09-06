70718cbcb39af3f98032b8d5ccfb59223a9f66e8
Eliot <saltycrane@gmail.com>
2022-09-06 10:43:56 -0700

    copy code to create 2 pages
    Copy code to create 2 pages: /non-optimized-grid and /optimized-grid.
    Change index page to link to the 2 grid pages. Note: the /optimized-grid
    page has not been optimized yet.

--- src/IndexPage.tsx
+++ src/IndexPage.tsx
@@ -1,89 +1,25 @@
-import { range } from "lodash";
-import { useState } from "react";
+import Link from "next/link";
 
-/**
- * IndexPage
- */
 export default function IndexPage() {
-  const [totalCount, setTotalCount] = useState(0);
-
-  const handleIncrement = () => {
-    setTotalCount((totalCount) => totalCount + 1);
-  };
-
   return (
     <div
       style={{
         padding: 10,
       }}
     >
-      <TotalCount totalCount={totalCount} />
-      <Grid onIncrement={handleIncrement} />
-    </div>
-  );
-}
-
-/**
- * Grid
- */
-type TGridProps = {
-  onIncrement: () => void;
-};
-
-function Grid({ onIncrement }: TGridProps) {
-  return (
-    <div
-      style={{
-        display: "grid",
-        gap: 10,
-        gridTemplateColumns: "repeat(10, 1fr)",
-      }}
-    >
-      {range(100).map((index) => {
-        return <Cell index={index} key={index} onIncrement={onIncrement} />;
-      })}
-    </div>
-  );
-}
-
-/**
- * Cell
- */
-type TCellProps = {
-  index: number;
-  onIncrement: () => void;
-};
-
-function Cell({ index, onIncrement }: TCellProps) {
-  const [count, setCount] = useState(0);
-
-  const handleClick = () => {
-    setCount((count) => count + 1);
-    onIncrement();
-  };
-
-  return (
-    <div style={{ border: "1px solid #ccc", padding: 5 }}>
-      <small>
-        <div>Cell {index}</div>
-        <button onClick={handleClick}>Increment</button>
-        <div>Count: {count}</div>
-      </small>
-    </div>
-  );
-}
-
-/**
- * TotalCount
- */
-type TTotalCountProps = {
-  totalCount: number;
-};
-
-function TotalCount({ totalCount }: TTotalCountProps) {
-  return (
-    <div style={{ marginBottom: 10 }}>
-      <div>Total count: {totalCount}</div>
+      <h1>react-re-render-example</h1>
+      <ul>
+        <li>
+          <Link href="/non-optimized-grid">
+            <a>Non-optimized Grid</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/optimized-grid">
+            <a>Optimized Grid</a>
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
---
+++ src/OptimizedGridPage.tsx
@@ -0,0 +1,90 @@
+import { range } from "lodash";
+import { useState } from "react";
+
+/**
+ * OptimizedGridPage
+ */
+export default function OptimizedGridPage() {
+  const [totalCount, setTotalCount] = useState(0);
+
+  const handleIncrement = () => {
+    setTotalCount((totalCount) => totalCount + 1);
+  };
+
+  return (
+    <div
+      style={{
+        padding: 10,
+      }}
+    >
+      <h1>Optimized Grid (not yet)</h1>
+      <TotalCount totalCount={totalCount} />
+      <Grid onIncrement={handleIncrement} />
+    </div>
+  );
+}
+
+/**
+ * Grid
+ */
+type TGridProps = {
+  onIncrement: () => void;
+};
+
+function Grid({ onIncrement }: TGridProps) {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gap: 10,
+        gridTemplateColumns: "repeat(10, 1fr)",
+      }}
+    >
+      {range(100).map((index) => {
+        return <Cell index={index} key={index} onIncrement={onIncrement} />;
+      })}
+    </div>
+  );
+}
+
+/**
+ * Cell
+ */
+type TCellProps = {
+  index: number;
+  onIncrement: () => void;
+};
+
+function Cell({ index, onIncrement }: TCellProps) {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount((count) => count + 1);
+    onIncrement();
+  };
+
+  return (
+    <div style={{ border: "1px solid #ccc", padding: 5 }}>
+      <small>
+        <div>Cell {index}</div>
+        <button onClick={handleClick}>Increment</button>
+        <div>Count: {count}</div>
+      </small>
+    </div>
+  );
+}
+
+/**
+ * TotalCount
+ */
+type TTotalCountProps = {
+  totalCount: number;
+};
+
+function TotalCount({ totalCount }: TTotalCountProps) {
+  return (
+    <div style={{ marginBottom: 10 }}>
+      <div>Total count: {totalCount}</div>
+    </div>
+  );
+}
