70a08b147d925852cde04f9022202d5366cf1fb4
Eliot <saltycrane@gmail.com>
2022-09-06 09:38:15 -0700

    add counter in each cell

--- src/IndexPage.tsx
+++ src/IndexPage.tsx
@@ -1,4 +1,5 @@
 import { range } from "lodash";
+import { useState } from "react";
 
 /**
  * IndexPage
@@ -15,18 +16,40 @@ function Grid() {
     <div
       style={{
         display: "grid",
+        gap: 10,
         gridTemplateColumns: "repeat(10, 1fr)",
         height: "100%",
         padding: 10,
       }}
     >
       {range(100).map((index) => {
-        return (
-          <div key={index} style={{ border: "1px solid #ccc" }}>
-            <small>Cell {index}</small>
-          </div>
-        );
+        return <Cell index={index} key={index} />;
       })}
+    </div>
+  );
+}
+
+/**
+ * Cell
+ */
+type TCellProps = {
+  index: number;
+};
+
+function Cell({ index }: TCellProps) {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount((count) => count + 1);
+  };
+
+  return (
+    <div style={{ border: "1px solid #ccc", padding: 5 }}>
+      <small>
+        <div>Cell {index}</div>
+        <button onClick={handleClick}>Increment</button>
+        <div>Count: {count}</div>
+      </small>
     </div>
   );
 }
